7787bfd6cd7e4199c071049c4bf16e8401655038
weihao0724 <weihao072401@1utar.my>
2024-09-01 20:28:35 +0800

    Implement JACOCO for test coverage report

--- test.sh
+++ test.sh
@@ -75,11 +75,11 @@ main() {
     # Building Docker images
     docker build --no-cache --pull --build-arg VERSION_TAG=alpha -t frooodle/s-pdf:latest -f ./Dockerfile .
     docker build --no-cache --pull --build-arg VERSION_TAG=alpha -t frooodle/s-pdf:latest-ultra-lite -f ./Dockerfile-ultra-lite .
-	
+
     # Test each configuration
     run_tests "Stirling-PDF-Ultra-Lite" "./exampleYmlFiles/docker-compose-latest-ultra-lite.yml"
 	docker-compose -f "./exampleYmlFiles/docker-compose-latest-ultra-lite.yml" down
-	
+
 
     run_tests "Stirling-PDF" "./exampleYmlFiles/docker-compose-latest.yml"
 	if [ $? -eq 0 ]; then
@@ -108,17 +108,17 @@ main() {
     docker build --no-cache --pull --build-arg VERSION_TAG=alpha -t frooodle/s-pdf:latest -f ./Dockerfile .
     docker build --no-cache --pull --build-arg VERSION_TAG=alpha -t frooodle/s-pdf:latest-ultra-lite -f ./Dockerfile-ultra-lite .
     docker build --no-cache --pull --build-arg VERSION_TAG=alpha -t frooodle/s-pdf:latest-fat -f ./Dockerfile-fat .
-    
-    
+
+
     # Test each configuration with security
     run_tests "Stirling-PDF-Ultra-Lite-Security" "./exampleYmlFiles/docker-compose-latest-ultra-lite-security.yml"
 	docker-compose -f "./exampleYmlFiles/docker-compose-latest-ultra-lite-security.yml" down
     run_tests "Stirling-PDF-Security" "./exampleYmlFiles/docker-compose-latest-security.yml"
 	docker-compose -f "./exampleYmlFiles/docker-compose-latest-security.yml" down
-	
+
 	run_tests "Stirling-PDF-Security-Fat" "./exampleYmlFiles/docker-compose-latest-fat-security.yml"
 	docker-compose -f "./exampleYmlFiles/docker-compose-latest-fat-security.yml" down
-	
+
     # Report results
     echo "All tests completed in $SECONDS seconds."
 
